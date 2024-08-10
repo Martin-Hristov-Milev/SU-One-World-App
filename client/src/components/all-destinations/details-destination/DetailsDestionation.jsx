044b577fa43a6b33cc9003cdef4a7ca9be4df96a
Martin Milev <142486974+Martin-Hristov-Milev@users.noreply.github.com>
2024-08-10 18:22:15 +0300

    Create Destination Component and style improvements

--- client/src/components/all-destinations/details-destination/DetailsDestionation.jsx
+++ client/src/components/all-destinations/details-destination/DetailsDestionation.jsx
@@ -10,14 +10,12 @@ export default function DetailsDestination(){
 
                 {/*<!--Content Left Side-->*/}
                 <div className="col-3">
-                    {/*<!--User Testimonial-->*/}
                     <blockquote className="testimonial text-right bigtest">
                             <q>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                                 labore et dolore magna aliqua
                             </q>
                             <footer>— John Doe, Happy Customer</footer>
                     </blockquote>
-                    {/*<!-- End of Testimonial-->*/}
 
                 </div>
                 
@@ -25,26 +23,65 @@ export default function DetailsDestination(){
                 <div className="col-3">
                     <div className="section-heading">
                         <h3>BELIEVING</h3>
-                        <h2 className="section-title">Focusing On What Matters Most</h2>
-                        <p className="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!</p>
+                        <h2 className="section-title"> Location , like City and Country</h2>
+                        
                     </div>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-                        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-                        dicta sunt explicabo.
+                    <p>Budget in USD
                     </p>
                     <p>
-                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                        Detailed description: Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
                         consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
                         Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet!
                     </p>
-                    <div>
-                    <a href="#"  className="button video link-lightbox">
+                    
+                    <a href="#"  className="button link-lightbox">
                         EDIT
-                    </a></div>
-                    <a href="#"  className="button video link-lightbox">
+                    </a>
+                    <a href="#"  className="button link-lightbox">
                         DELETE
                     </a>
+
+                    <div >
+                    <h4>COMMENTS:</h4>
+                         <ul> 
+                            <li>
+                                <p>user: comment one </p>
+                            </li>
+                            <li>
+                                <p>user: comment one </p>
+                            </li>
+                                {/* {comments.map( ({_id, author, text}) => (
+
+                                    <li key={_id} className="comment" >
+                                        <p>{author.email}: {text}</p>
+                                    </li>
+                                ) )
+                                } */}
+                         </ul>
+                            {/* {comments.length === 0 && (
+                                <p className="no-comment">No comment.</p>
+                                )} */}
+                    </div> 
+                    <article >
+                      <label>Add new comment:</label>
+
+                    <form 
+                    // onSubmit={submitHandler}
+                    >
+
+                    {/* <input type= 'text' name='username' placeholder="username..."  /> */}
+
+                    <textarea 
+                        name="comment" 
+                        placeholder="Tell us what you think ....."
+                        // onChange = {changeHandler}
+                        // value = {values.comment}
+                    ></textarea>
+
+                    <input className="button submit" type="submit" value="Add Comment"/>
+                    </form>
+                    </article>
+
                 </div>
                 {/*<!--End Content Right Side-->*/}
 
@@ -53,8 +90,6 @@ export default function DetailsDestination(){
                 </div>
 
             </div>
-
-
         </div>
     </div>
     )
--- client/src/components/create-destination/CreateDestination.jsx
+++ client/src/components/create-destination/CreateDestination.jsx
@@ -1,5 +1,8 @@
 
 import { Link } from "react-router-dom"
+
+
+
 export default function CreateDestination(){
 
 
